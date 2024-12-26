74628b3176f1866a49954e6fecef26df9c65667d
nmoisesafantec <nmoises@afantec.com>
2024-12-26 10:40:11 -0300

    se agrego el enum de merchant_type

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Types\MerchantType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Auditable;
@@ -24,6 +25,11 @@ class Merchant extends Model implements ContractsAuditable
         'merchant_id',
         'locality',
         'address'
+    ];
+
+    protected $casts = [
+        'merchant_type' => MerchantType::class,
+        
     ];
 
     public function parentMerchant()
---
+++ app/Types/MerchantType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Types;
+
+enum MerchantType: string
+{
+    case TENANT = 'tenat';
+    case CLIENT = 'client'; 
+    
+}
