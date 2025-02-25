764fcb120ac882fa726e13746da892fd310b8b4c
niicodeer <nicoradin.job@gmail.com>
2025-02-25 01:19:02 -0300

    AGR-60 fix delete permissions

--- resources/views/pages/products/action.blade.php
+++ resources/views/pages/products/action.blade.php
@@ -1,12 +1,14 @@
-@can('products.edit')
 <div class="btn-group">
+    @can('products.edit')
     <a href="{{ route('products.edit', $id) }}" class="btn btn-sm btn-alt-secondary js-bs-tooltip-enabled" data-bs-toggle="tooltip" aria-label="Edit" data-bs-original-title="{{ __('crud.items.actions.create') }}">
         <i class="fa fa-fw fa-pencil-alt text-primary"></i>
     </a>
+    @endcan
+    @can('products.destroy')
     <x-delete-action
         :route="route('products.destroy', $model->id)"
         :id="$model->id"
         :title="$model->name"   
         :model="$model::class" /> 
+    @endcan
 </div>
-@endcan
