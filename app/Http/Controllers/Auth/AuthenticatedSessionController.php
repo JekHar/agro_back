5fc8d66532696f0b98f91cb13ebcd8fb236f7a11
nmoisesafantec <nmoises@afantec.com>
2025-07-31 13:09:21 -0300

    fix sidebar y redirect login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect('/clients/merchants');
+        return redirect('/orders');
     }
 
     /**
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -57,43 +57,51 @@
             <ul class="nav-main">
                 <!-- GESTIÓN -->
                 <li class="nav-main-heading">GESTIÓN</li>
+                @can('orders.index')
+                    <li class="nav-main-item">
+                        <a class="nav-main-link{{ request()->is('orders*') ? ' active' : '' }}"
+                            href="{{ route('orders.index') }}">
+                            <i class="nav-main-link-icon fa fa-clipboard"></i>
+                            <span class="nav-main-link-name">{{ __('crud.sidebar.orders') }}</span>
+                        </a>
+                    </li>
+                @endcan
                 @can('clients.merchants.index')
-                <li class="nav-main-item{{ request()->is('clients/*') || request()->is('lots/*') ? ' open' : '' }}">
-                    <a class="nav-main-link nav-main-link-submenu{{ request()->is('clients/*') || request()->is('lots/*') ? ' active' : '' }}"
-                       data-toggle="submenu" aria-haspopup="true" aria-expanded="true"
-                       href="#">
-                        <i class="nav-main-link-icon fa fa-building-user"></i>
-                        <span class="nav-main-link-name">{{ __('crud.sidebar.clients') }}</span>
-                    </a>
-                    <ul class="nav-main-submenu">
-                        <li class="nav-main-item">
-                            <a class="nav-main-link{{ request()->is('clients/*') ? ' active' : '' }}"
-                               href="{{ route('clients.merchants.index') }}">
-                               <i class="nav-main-link-icon fa fa-building-user"></i>
-                                <span class="nav-main-link-name">{{ __('crud.sidebar.clients') }}</span>
-                            </a>
-                        </li>
-                        @can('tenants.merchants.index')
-                        <li class="nav-main-item">
-                            <a class="nav-main-link{{ request()->is('tenants/*') ? ' active' : '' }}"
-                               href="{{ route('tenants.merchants.index') }}">
-                               <i class="nav-main-link-icon fa fa-building-wheat"></i>
-                                <span class="nav-main-link-name">{{ __('crud.sidebar.tenants') }}</span>
-                            </a>
-                        </li>
-                        @endcan
-                        @can('lots.index')
-                        <li class="nav-main-item">
-                            <a class="nav-main-link{{ request()->is('lots*') ? ' active' : '' }}"
-                               href="{{ route('lots.index') }}">
-                               <i class="nav-main-link-icon fa fa-map-location-dot"></i>
-                                <span class="nav-main-link-name">{{ __('crud.sidebar.lots') }}</span>
-                            </a>
-                        </li>
-                        @endcan
+                    <li class="nav-main-item{{ request()->is('clients/*') || request()->is('lots/*') ? ' open' : '' }}">
+                        <a class="nav-main-link nav-main-link-submenu{{ request()->is('clients/*') || request()->is('lots/*') ? ' active' : '' }}"
+                            data-toggle="submenu" aria-haspopup="true" aria-expanded="true" href="#">
+                            <i class="nav-main-link-icon fa fa-building-user"></i>
+                            <span class="nav-main-link-name">{{ __('crud.sidebar.clients') }}</span>
+                        </a>
+                        <ul class="nav-main-submenu">
+                            <li class="nav-main-item">
+                                <a class="nav-main-link{{ request()->is('clients/*') ? ' active' : '' }}"
+                                    href="{{ route('clients.merchants.index') }}">
+                                    <i class="nav-main-link-icon fa fa-building-user"></i>
+                                    <span class="nav-main-link-name">{{ __('crud.sidebar.clients') }}</span>
+                                </a>
+                            </li>
+                            @can('tenants.merchants.index')
+                                <li class="nav-main-item">
+                                    <a class="nav-main-link{{ request()->is('tenants/*') ? ' active' : '' }}"
+                                        href="{{ route('tenants.merchants.index') }}">
+                                        <i class="nav-main-link-icon fa fa-building-wheat"></i>
+                                        <span class="nav-main-link-name">{{ __('crud.sidebar.tenants') }}</span>
+                                    </a>
+                                </li>
+                            @endcan
+                            @can('lots.index')
+                                <li class="nav-main-item">
+                                    <a class="nav-main-link{{ request()->is('lots*') ? ' active' : '' }}"
+                                        href="{{ route('lots.index') }}">
+                                        <i class="nav-main-link-icon fa fa-map-location-dot"></i>
+                                        <span class="nav-main-link-name">{{ __('crud.sidebar.lots') }}</span>
+                                    </a>
+                                </li>
+                            @endcan
 
-                    </ul>
-                </li>
+                        </ul>
+                    </li>
                 @endcan
                 {{-- @can('lots.index')
                 <li class="nav-main-item">
@@ -104,72 +112,64 @@
                     </a>
                 </li>
                 @endcan --}}
-                @can('orders.index')
-                <li class="nav-main-item">
-                    <a class="nav-main-link{{ request()->is('orders*') ? ' active' : '' }}"
-                        href="{{ route('orders.index') }}">
-                        <i class="nav-main-link-icon fa fa-clipboard"></i>
-                        <span class="nav-main-link-name">{{ __('crud.sidebar.orders') }}</span>
-                    </a>
-                </li>
-                @endcan
-            
+
+
                 <!-- ADMINISTRACIÓN -->
                 <li class="nav-main-heading">ADMINISTRACIÓN</li>
                 @can('users.index')
-                <li class="nav-main-item">
-                    <a class="nav-main-link{{ request()->is('users*') ? ' active' : '' }}"
-                        href="{{ route('users.index') }}">
-                        <i class="nav-main-link-icon si si-user"></i>
-                        <span class="nav-main-link-name">{{ __('crud.sidebar.users') }}</span>
-                    </a>
-                </li>
+                    <li class="nav-main-item">
+                        <a class="nav-main-link{{ request()->is('users*') ? ' active' : '' }}"
+                            href="{{ route('users.index') }}">
+                            <i class="nav-main-link-icon si si-user"></i>
+                            <span class="nav-main-link-name">{{ __('crud.sidebar.users') }}</span>
+                        </a>
+                    </li>
                 @endcan
                 @can('services.index')
-                <li class="nav-main-item">
-                    <a class="nav-main-link{{ request()->is('services*') ? ' active' : '' }}"
-                        href="{{ route('services.index') }}">
-                        <i class="nav-main-link-icon si si-wrench"></i>
-                        <span class="nav-main-link-name">{{ __('crud.sidebar.services') }}</span>
-                    </a>
-                </li>
+                    <li class="nav-main-item">
+                        <a class="nav-main-link{{ request()->is('services*') ? ' active' : '' }}"
+                            href="{{ route('services.index') }}">
+                            <i class="nav-main-link-icon si si-wrench"></i>
+                            <span class="nav-main-link-name">{{ __('crud.sidebar.services') }}</span>
+                        </a>
+                    </li>
                 @endcan
                 @can('products.index')
-                <li class="nav-main-item{{ request()->is('products*') || request()->is('categories*') ? ' open' : '' }}">
-                    <a class="nav-main-link nav-main-link-submenu{{ request()->is('products*') || request()->is('categories*') ? ' active' : '' }}"
-                       data-toggle="submenu" aria-haspopup="true" aria-expanded="true"
-                       href="#">
-                        <i class="nav-main-link-icon fa fa-bottle-water"></i>
-                        <span class="nav-main-link-name">{{ __('crud.sidebar.products') }}</span>
-                    </a>
-                    <ul class="nav-main-submenu">
-                        <li class="nav-main-item">
-                            <a class="nav-main-link{{ request()->is('products*') ? ' active' : '' }}"
-                               href="{{ route('products.index') }}">
-                                <i class="nav-main-link-icon fa fa-bottle-water"></i>
-                                <span class="nav-main-link-name">{{ __('crud.sidebar.products') }}</span>
-                            </a>
-                        </li>
-                        @can('categories.index')
-                        <li class="nav-main-item">
-                            <a class="nav-main-link{{ request()->is('categories*') ? ' active' : '' }}"
-                               href="{{ route('categories.index') }}">
-                                <i class="nav-main-link-icon si si-grid"></i>
-                                <span class="nav-main-link-name">{{ __('crud.sidebar.categories') }}</span>
-                            </a>
-                        </li>
-                        @endcan
-                    </ul>
-                </li>
+                    <li
+                        class="nav-main-item{{ request()->is('products*') || request()->is('categories*') ? ' open' : '' }}">
+                        <a class="nav-main-link nav-main-link-submenu{{ request()->is('products*') || request()->is('categories*') ? ' active' : '' }}"
+                            data-toggle="submenu" aria-haspopup="true" aria-expanded="true" href="#">
+                            <i class="nav-main-link-icon fa fa-bottle-water"></i>
+                            <span class="nav-main-link-name">{{ __('crud.sidebar.products') }}</span>
+                        </a>
+                        <ul class="nav-main-submenu">
+                            <li class="nav-main-item">
+                                <a class="nav-main-link{{ request()->is('products*') ? ' active' : '' }}"
+                                    href="{{ route('products.index') }}">
+                                    <i class="nav-main-link-icon fa fa-bottle-water"></i>
+                                    <span class="nav-main-link-name">{{ __('crud.sidebar.products') }}</span>
+                                </a>
+                            </li>
+                            @can('categories.index')
+                                <li class="nav-main-item">
+                                    <a class="nav-main-link{{ request()->is('categories*') ? ' active' : '' }}"
+                                        href="{{ route('categories.index') }}">
+                                        <i class="nav-main-link-icon si si-grid"></i>
+                                        <span class="nav-main-link-name">{{ __('crud.sidebar.categories') }}</span>
+                                    </a>
+                                </li>
+                            @endcan
+                        </ul>
+                    </li>
                 @endcan
                 @can('aircrafts.index')
-                <li class="nav-main-item">
-                    <a class="nav-main-link{{ request()->is('aircrafts*') ? ' active' : '' }}"
-                        href="{{ route('aircrafts.index') }}">
-                        <i class="nav-main-link-icon si si-plane"></i>
-                        <span class="nav-main-link-name">{{ __('crud.sidebar.aircrafts') }}</span>
-                    </a>
-                </li>
+                    <li class="nav-main-item">
+                        <a class="nav-main-link{{ request()->is('aircrafts*') ? ' active' : '' }}"
+                            href="{{ route('aircrafts.index') }}">
+                            <i class="nav-main-link-icon si si-plane"></i>
+                            <span class="nav-main-link-name">{{ __('crud.sidebar.aircrafts') }}</span>
+                        </a>
+                    </li>
                 @endcan
             </ul>
         </div>
